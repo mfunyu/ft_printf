170dbd52511a733b62d0a3ae14c1019f865b9414
mfunyu <you@example.com>
2023-10-02 17:27:53 +0200

    fix: includes

--- ft_printf.h
+++ ft_printf.h
@@ -6,17 +6,17 @@
 /*   By: mfunyu <mfunyu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 10:45:21 by mfunyu            #+#    #+#             */
-/*   Updated: 2023/09/20 14:54:54 by mfunyu           ###   ########.fr       */
+/*   Updated: 2023/10/02 17:27:26 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft.h"
+# include <unistd.h>
 
 # ifdef DEBUG
-#  define FILENO STDERR_FILENO 
+#  define FILENO STDERR_FILENO
 # else
 #  define FILENO STDOUT_FILENO
 # endif
